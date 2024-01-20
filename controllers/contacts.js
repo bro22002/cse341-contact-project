45417d9f1e4aefa7c858cdede2d3ce5e2e5dc2ef
Mr. Brown <bro22002@byui.edu>
2024-01-20 21:59:20 +0000

    Added the PUT, POST, and DELETE routes

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,6 +2,7 @@ const mongodb = require("../db/database")
 const ObjectId = require("mongodb").ObjectId
 
 const getAll = async (req, res) => {
+    //#swagger.tags=['Contacts']
     const result = await mongodb.getDatabase().collection('contacts').find()
     result.toArray().then((contacts) => {
         res.setHeader('Content-Type', 'application/json')
@@ -10,6 +11,7 @@ const getAll = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
+    //#swagger.tags=['Contacts']
     const userId = new ObjectId(req.params.id)
     const result = await mongodb.getDatabase().collection('contacts').find({_id: userId});
     result.toArray().then((contacts) => {
@@ -18,4 +20,52 @@ const getSingle = async (req, res) => {
     });
 };
 
-module.exports = {getAll, getSingle}
+const createContact = async (req, res) => {
+    //#swagger.tags=['Contacts']
+    const contact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    }
+    const result = await mongodb.getDatabase().collection('contacts').insertOne({contact});
+    if (result.acknowledged) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(result.error || "Some error occured while updating the user.")
+    }
+    
+};
+
+const updateContact = async (req, res) => {
+    //#swagger.tags=['Contacts']
+    const userId = new ObjectId(req.params.id)
+    const contact = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday
+    }
+    const result = await mongodb.getDatabase().collection('contacts').replaceOne({_id: userId}, contact);
+    if (result.modifiedCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(result.error || "Some error occured while updating the user.");
+    }
+};
+
+const deleteContact = async (req, res) => {
+    //#swagger.tags=['Contacts']
+    const userId = new ObjectId(req.params.id)
+    const result = await mongodb.getDatabase().collection('contacts').deleteOne({_id: userId});
+    if (result.deleteCount > 0) {
+        res.status(204).send();
+    } else {
+        res.status(500).json(result.error || "Some error occured while updating the user.");
+    }
+
+};
+
+module.exports = {getAll, getSingle, createContact, updateContact, deleteContact}
